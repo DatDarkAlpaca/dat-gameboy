fa22e8665fb942edbbfe81f84df6a9fa7b1a4348
Alpaca <paulocastrogalvao@hotmail.com>
2024-12-10 16:29:00 +0900

    Removes urls from main.cpp

--- dat-gameboy/src/main.cpp
+++ dat-gameboy/src/main.cpp
@@ -1,14 +1,6 @@
 #include "pch.hpp"
 #include "application.hpp"
 
-#include <iostream>
-
-// https://gbdev.io/gb-opcodes/optables/
-// https://rgbds.gbdev.io/docs/v0.8.0/gbz80.7#ADC_A,r8
-// Game Boy: Complete Technical Reference (https://gekkio.fi)
-// https://gbdev.io/pandocs/CPU_Registers_and_Flags.html
-// https://gbdev.gg8.se/wiki/articles/Gameboy_Bootstrap_ROM#The_.27Pokemon.27_CGB_bootstrap
-
 int main(int argc, char** argv)
 {
 	dat::initialize_logger();
